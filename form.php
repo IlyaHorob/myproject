d591dafd1f174884ac6fde789464e474ae91c63c
Ilya <hibestaukro@gmail.com>
2017-04-01 10:44:26 +0300

    upload files dz6

---
+++ form.php
@@ -0,0 +1,47 @@
+<?php include_once "handler.php";?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>DZ-6</title>
+</head>
+<body>
+<form method="post" action="form.php">
+    <table width="70%" align="center" border="1">
+        <tr>
+            <td>
+                <label>Date1:
+                    <input type="text" name="date1" value="" placeholder="YYYY-mm-dd">
+                </label>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label>Date2:
+                    <input type="text" name="date2" value="" placeholder="YYYY-mm-dd">
+                </label>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label>dd.mm.YY :
+                    <input type="radio" name="format" value="d.m.Y" checked="checked">
+                </label>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label>YY.mm.dd :
+                    <input type="radio" name="format" value="Y.m.d">
+                </label>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="submit" value="Send">
+            </td>
+        </tr>
+    </table>
+</form>
+</body>
+</html>
